4198432c347d090a1f3b27e3a04f2619e2f3f007
kaue-guerra <kaueguerra@gmail.com>
2022-01-07 23:13:22 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Content } from './styles';
+
+
+export function Header(){
+    return(
+        <Container>
+            <Content>
+                <h3>Codelândia</h3>
+            </Content>
+        </Container>
+    )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'  
+
+export const Container = styled.header`
+    position: absolute;
+    width: 100%;
+    height: 250px;
+    background: linear-gradient(88.27deg, #574AE8 0%, #3EA1DB 100%);
+
+`
+
+export const Content = styled.div`
+        max-width: 900px;
+        margin: 0 auto;
+        color: #fff;
+
+`
